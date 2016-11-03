c74fff0372d575ef973dc2ff8948f12dd18319bf
ZXY <279640804@qq.com>
2016-11-03 22:03:13 +0800

    abc

--- assets/app.js
+++ assets/app.js
@@ -65,7 +65,7 @@
 /******/ 	}
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "58d73e23cc959df84196"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "ff0ca3cbbd97e635b450"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 /******/ 	
@@ -570,7 +570,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/assets/";
+/******/ 	__webpack_require__.p = "assets/";
 /******/
 /******/ 	// __webpack_hash__
 /******/ 	__webpack_require__.h = function() { return hotCurrentHash; };
